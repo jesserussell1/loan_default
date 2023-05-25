9abe0e2e4e002550bdad25fd218261331636a9fc
Jesse Russell <jesse.r.russell@gmail.com>
2023-05-25 16:17:15 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import  LabelEncoder
+import xgboost as xgb
+import numpy as np
+st.header("Loan Default Prediction App")
+st.text_input("Enter your Name: ", key="name")
+data = pd.read_csv("defaults_data.csv")
+#load label encoder
+encoder = LabelEncoder()
+encoder.classes_ = np.load('classes.npy',allow_pickle=True)
+
+# load model
+best_xgboost_model = xgb.XGBRegressor()
+best_xgboost_model.load_model("best_model.json")
+
+if st.checkbox('Show Training Dataframe'):
+    data
+
+st.subheader("Please select relevant features.")
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_gender = st.radio(
+        'Gender:',
+        np.unique(data['SEX']))
+
+    inp_marriage = st.radio(
+        'Marriage:',
+        np.unique(data['MARRIAGE']))
+
+    inp_education= st.radio(
+        'Education:',
+        np.unique(data['EDUCATION']))
+
+input_credit_limit = st.slider('Credit limit', 0.0, max(data["LIMIT_BAL"]), 1.0)
+input_bill = st.slider('Most recent bill amount', 0.0, max(data["BILL_AMT6"]), 1.0)
+input_payment = st.slider('Most recent payment amount', 0.0, max(data["PAY_AMT6"]), 1.0)
+input_age = st.slider('Age', 0.0, max(data["AGE"]), 1.0)
+
+
+
+if st.button('Make Prediction'):
+    inp_gender = encoder.transform(np.expand_dims(inp_gender, -1))
+    inp_marriage = encoder.transform(np.expand_dims(inp_marriage, -1))
+    inp_education = encoder.transform(np.expand_dims(inp_education, -1))
+
+    inputs = np.expand_dims(
+        [int(inp_gender), int(inp_marriage), int(inp_education),
+         input_credit_limit, input_bill, input_payment, input_age], 0)
+    prediction = best_xgboost_model.predict(inputs)
+    print("final pred", np.squeeze(prediction, -1))
+    st.write(f"Your prediction is: {np.squeeze(prediction, -1):.2f}g")
+
+    st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
+
---
+++ prediction.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import  LabelEncoder
+import xgboost as xgb
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+def evauation_model(pred, y_val):
+  score_MSE = round(mean_squared_error(pred, y_val),2)
+  score_MAE = round(mean_absolute_error(pred, y_val),2)
+  score_r2score = round(r2_score(pred, y_val),2)
+  return score_MSE, score_MAE, score_r2score
+
+
+data = pd.read_csv("defaults_data.csv")
+
+y = data['default']
+x_train, x_test, y_train, y_test = train_test_split(data, y, test_size=0.2, random_state=42)
+label_encoder = LabelEncoder()
+x_train['SEX'] = label_encoder.fit_transform(x_train['SEX'].values)
+x_test['SEX'] = label_encoder.transform(x_test['SEX'].values)
+x_train['EDUCATION'] = label_encoder.fit_transform(x_train['EDUCATION'].values)
+x_test['EDUCATION'] = label_encoder.transform(x_test['EDUCATION'].values)
+x_train['MARRIAGE'] = label_encoder.fit_transform(x_train['MARRIAGE'].values)
+x_test['MARRIAGE'] = label_encoder.transform(x_test['MARRIAGE'].values)
+
+#save label encoder classes
+np.save('classes.npy', label_encoder.classes_)
+
+# load model
+best_xgboost_model = xgb.XGBRegressor()
+best_xgboost_model.load_model("best_model.json")
+pred = best_xgboost_model.predict(x_test)
+score_MSE, score_MAE, score_r2score = evauation_model(pred, y_test)
+print(score_MSE, score_MAE, score_r2score)
+#%%
+loaded_encoder = LabelEncoder()
+loaded_encoder.classes_ = np.load('classes.npy',allow_pickle=True)
+print(x_test.shape)
+input_default = loaded_encoder.transform(np.expand_dims("0",-1))
+print(int(input_default))
+inputs = np.expand_dims([int(input_default),0,1,1,1,24,0, 0],0)
+print(inputs.shape)
+prediction = best_xgboost_model.predict(inputs)
+print("final pred", np.squeeze(prediction,-1))
+
